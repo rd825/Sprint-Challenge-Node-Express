cccc5006eb0de110c290dc99478426deef4047df
Rahul Desai <rd825@georgetown.edu>
2018-11-09 13:34:55 -0500

    Completed error handling

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const actionDB = require('../data/helpers/actionModel');
+const projectDB = require('../data/helpers/projectModel');
 
 // custom middleware
 const actionCheck = require('../config/actionCheck');
@@ -31,10 +32,13 @@ router.post('/', actionCheck, (req, res) => {
     if (!project_id || !description || !notes) {
         res.status(400).json({message: "Please provide a project id, description, and notes for the action"});
     } else {
-        actionDB.insert(req.body)
+        projectDB.get(project_id).then(project => {
+            actionDB.insert(req.body)
               .then(action => {
                 res.status(201).json(action);
               }).catch(err => {res.status(500).json({message: "There was an error saving the action to the database"})})
+        })
+        .catch (err => res.status(404).json({message: "The project for which you've provided an ID does not exist"}))
     }
 })
 
@@ -57,17 +61,20 @@ router.put('/:id', actionCheck, (req, res) => {
     if (!project_id || !description || !notes) {
         res.status(400).json({message: "Please provide a project id, description, and notes for the action"});
     } else {
-        actionDB.update(req.params.id, req.body)
-                .then(action => {
-                    if (action) {
-                        res.status(200).json(action)
-                    } else {
-                        res.status(404).json({message: "The action with the specified ID does not exist."});
-                    }
-                })
-                .catch(err => {
-                    res.status(500).json({error: "The action could not be removed"});
-                })
+        projectDB.get(project_id).then(project => {
+            actionDB.update(req.params.id, req.body)
+                    .then(action => {
+                        if (action) {
+                            res.status(200).json(action)
+                        } else {
+                            res.status(404).json({message: "The action with the specified ID does not exist."});
+                        }
+                    })
+                    .catch(err => {
+                        res.status(500).json({error: "The action could not be removed"});
+                    })
+        })
+        .catch (err => res.status(404).json({message: "The project for which you've provided an ID does not exist"}))
     }
 })
 
